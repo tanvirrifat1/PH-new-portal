2bc39eafa1c9fdd2356c4830dca8c4d9423ff76e
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-07-24 15:52:40 +0600

    created dynamic page

--- src/pages/news/[newsId].js
+++ src/pages/news/[newsId].js
@@ -1,10 +1,78 @@
 import RootLayout from "@/components/Layouts/RootLayout";
+import { Col, Row } from "antd";
+import Image from "next/image";
 import React from "react";
+import {
+  CalendarOutlined,
+  CommentOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
 
 const NewsDetailPage = ({ news }) => {
+  if (!news) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
-      <h1>{news.title}</h1>
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32,
+        }}
+      >
+        <Col className="gutter-row" span={12}>
+          <div>
+            <Image
+              src={news?.image_url}
+              width={800}
+              height={400}
+              responsive
+              alt="news image"
+            />
+          </div>
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <div>
+            <h1>title={news?.title}</h1>
+            <div
+              className="line"
+              style={{
+                height: "5px",
+                margin: "20px 0",
+                background: "#000",
+                width: "100%",
+              }}
+            ></div>
+
+            <p
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+                color: "gray",
+                margin: "10px 0px",
+                fontSize: "12px",
+              }}
+            >
+              <span>
+                <CalendarOutlined /> {news?.release_date}
+              </span>
+              <span>
+                <CommentOutlined /> {news?.comment_count} COMMENTS
+              </span>
+              <span>
+                <ProfileOutlined /> {news?.category}
+              </span>
+            </p>
+
+            <p style={{ fontSize: "20px" }}>{news?.description}</p>
+          </div>
+          <h1>Author: {news?.author}</h1>
+        </Col>
+      </Row>
     </div>
   );
 };
@@ -23,7 +91,7 @@ export const getStaticPaths = async () => {
     params: { newsId: news.id.toString() },
   }));
 
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 };
 
 export const getStaticProps = async (context) => {
