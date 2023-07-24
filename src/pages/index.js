50ecd2b17c980fd380f117762ec7f57173e745bf
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-07-24 23:11:03 +0600

    added dynamic lazy loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
 import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
-import Banner from "@/components/UI/Banner";
+// import Banner from "@/components/UI/Banner";
 import AllNews from "@/components/UI/AllNews";
 import { useGetNewsesQuery } from "@/redux/api/api";
+import dynamic from "next/dynamic";
 
 const HomePage = () => {
-  const { data, isLoading, isError } = useGetNewsesQuery();
+  const { data } = useGetNewsesQuery();
 
-  if (isLoading) {
-    <p>Loading...</p>;
-  }
+  const DynamicBanner = dynamic(() => import("@/components/UI/Banner"), {
+    loading: () => <p>Loading...</p>,
+  });
+
   return (
     <>
       <Head>
@@ -21,7 +23,7 @@ const HomePage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Banner />
+      <DynamicBanner />
       <AllNews allNews={data} />
     </>
   );
