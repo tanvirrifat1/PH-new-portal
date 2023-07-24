71e942438171c3a1b82a7a80c20163043b24cfbd
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-07-24 14:35:57 +0600

    added revalidate opting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
 import Banner from "@/components/UI/Banner";
+import AllNews from "@/components/UI/AllNews";
 
 const HomePage = ({ allNews }) => {
-  console.log(allNews);
   return (
     <>
       <Head>
@@ -16,6 +16,7 @@ const HomePage = ({ allNews }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Banner />
+      <AllNews allNews={allNews} />
     </>
   );
 };
@@ -34,5 +35,6 @@ export const getStaticProps = async () => {
     props: {
       allNews: data,
     },
+    revalidate: 10,
   };
 };
